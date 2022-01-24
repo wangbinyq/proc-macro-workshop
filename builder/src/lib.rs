86463e5e121e8b45938b98f71d848316c05028ed
wangb <bin.wang@idiaoyan.com>
2022-01-24 10:28:14 +0800

    pass test01: add builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,19 @@
-use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+
+    let expanded = quote! {
+        pub struct #builder_name {
+
+        }
+    };
+
+    proc_macro::TokenStream::from(expanded)
 }
