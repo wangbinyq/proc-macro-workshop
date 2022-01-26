748144158c4eaf4ed752464de02f6aebcb61d1ab
wangb <bin.wang@idiaoyan.com>
2022-01-26 10:03:45 +0800

    seq pass test 1

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,31 @@
-use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse::{Parse, ParseStream, Result as ParseResult},
+    Ident, LitInt, Token,
+};
+
+struct Seq {
+    ident: Ident,
+    start: LitInt,
+    end: LitInt,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> ParseResult<Self> {
+        let ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let end = input.parse()?;
+        Ok(Self { ident, start, end })
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let _ = input;
 
-    unimplemented!()
+    let expand = quote! {};
+
+    proc_macro::TokenStream::from(expand)
 }
